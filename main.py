dfc8b5d02566ae3b056083570939d8d03ebf1d44
Cássio Reis <cassio.reis@engenharia.ufjf.br>
2022-01-31 15:41:35 -0300

    Uploading docker-compose.yml and Dockerfile

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-from pyspark.sql import SparkSession
+#hello world
+def main():
+    print("Hello World")
 
-
-def map_function(df):
-    return df.rdd.map(lambda x: (x[0], x[1]))
-
-
-def reduce_function(df):
-    return df.rdd.reduceByKey(lambda x, y: max(x, y))
-
-
-if '__name__' == '__main__':
-    spark = SparkSession.builder.appName('map_reduce').getOrCreate()
-    df = spark.read.csv('data/dados.csv', header=True, inferSchema=True)
-    df_map = map_function(df)
-    df_reduce = reduce_function(df)
-    df_map.show()
-    df_reduce.show()
-    spark.stop()
-
+main
